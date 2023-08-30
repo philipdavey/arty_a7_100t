1f9b32b24d68c8a66166f8a713c257b96412a87c
PhilipDavey <pjddavey@gmail.com>
2023-08-30 13:16:58 +0100

    added vga driver to script

--- synthesis/run_vivado.tcl
+++ synthesis/run_vivado.tcl
@@ -1,14 +1,25 @@
-# Configure Paths / Values
-##########################
 
+
+# Configure Paths / Values
+####################################################################
 set script_dir [pwd]
-set output dir $script_dir/vivado
+set output_dir $script_dir/vivado
 set source_dir ../source
 
 set project_name arty_a7_100t_vivado
 set part_num xc7a100tcsg324-1
 
 # Create Vivado Project:
+####################################################################
 create_project -force $project_name $output_dir -part $part_num
 
 # Add RTL files:
+####################################################################
+cd $source_dir
+
+# add_files -norecurse arty_top.vhd
+add_files -norecurse vga_driver/vga_driver.vhd
+
+# Add XDC file:
+####################################################################
+add_files -norecurse arty_a7_100_master.xdc
